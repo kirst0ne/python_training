85c4692c96cd367ff68760b4327b00253b48f39e
Kirill Seleznev <okolo66@yandex.ru>
2024-06-30 19:23:40 +0300

    Добавлен Parameter_Oblect

--- test_add_group.py
+++ test_add_group.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
 def is_alert_present(self):
     try:
@@ -33,20 +34,20 @@ class test_add_group(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, group_name, group_header, group_footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_id("content").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group_name)
+        wd.find_element_by_name("group_name").send_keys(group.group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group_header)
+        wd.find_element_by_name("group_header").send_keys(group.group_header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group_footer)
+        wd.find_element_by_name("group_footer").send_keys(group.group_footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -61,7 +62,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, group_name="Random", group_header="Random", group_footer="Random")
+        self.create_group(wd, Group(group_name="Random", group_header="Random", group_footer="Random"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -70,7 +71,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, group_name="", group_header="", group_footer="")
+        self.create_group(wd, Group(group_name="", group_header="", group_footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
