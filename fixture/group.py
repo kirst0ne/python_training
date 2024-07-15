c7e7733d6844a1b2e13f386f07145c5bd9a8734d
Kirill Seleznev <okolo66@yandex.ru>
2024-07-15 22:39:46 +0300

    Добавлены тесты модификации первых контактов и групп 2.0

--- fixture/group.py
+++ fixture/group.py
@@ -27,6 +27,28 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def modify_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # init group modify
+        wd.find_element_by_name("edit").click()
+        # modify group form
+        wd.find_element_by_id("content").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.group_name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.group_header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.group_footer)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
