ec276b6e00a98121de8457ffc88b344b4da5aee1
Kirill Seleznev <okolo66@yandex.ru>
2024-07-17 00:35:48 +0300

    Реализована валидация фикстуры перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,16 +3,12 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Kirill", lastname="Seleznev", middlename="Aleksandrovich",
                                nickname="kirts0ne", address="Repischeva st. 10, 149 flat",
                                mobile="+79119715279", email="okolo66@yandex.ru", bday="30",
                                bmounth="April", byear="1996"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", lastname="", middlename="", nickname="", address="",
                                mobile="", email="", bday="", bmounth="-", byear=""))
-    app.session.logout()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,10 +2,8 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.session.home_page()
     app.contact.modify_first_contact(Contact(firstname="Grezckii", lastname="Oleg", middlename="Viktorovich",
                                              nickname="oreh666", address="Vokzalnya st. 17, 49 flat",
                                              mobile="+79116660066", email="emaple@yandex.ru", bday="6",
                                              bmounth="January", byear="1998"))
-    app.session.logout()
