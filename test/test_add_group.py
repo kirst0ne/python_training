942febcab0e19297684d33985b5cc30a648d15ef
Kirill Seleznev <okolo66@yandex.ru>
2024-07-16 23:36:44 +0300

    Реализована валидация фикстуры перед каждым тестом

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,12 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(group_name="Random123", group_header="Random123", group_footer="Random123"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(group_name="", group_header="", group_footer=""))
-    app.session.logout()
