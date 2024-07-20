f17b88f1e680fb5b6db1bb0b01696fd93d11c53e
Kirill Seleznev <okolo66@yandex.ru>
2024-07-20 14:58:42 +0300

    Реализована проверка валидности фикстуры и проверка состояния сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,6 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(group_name="Random123", group_header="Random123", group_footer="Random123"))
-#    app.session.logout()
 
 
 def test_add_empty_group(app):
