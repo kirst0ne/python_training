647c5176fb0b3404cb025780ccd33625424a95f8
Kirill Seleznev <okolo66@yandex.ru>
2024-08-21 00:30:57 +0300

    Реализованы проверки путём сравнения списков в тестах для создания, удаления и модификации групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,12 +2,34 @@ from model.group import Group
 
 
 def test_modify_first_group_name(app):
+    old_groups = app.group.get_group_list()
+    group = Group(group_name="New group")
     if app.group.count() == 0:
-        app.group.create(Group(group_name="test"))
-    app.group.modify_first_group(Group(group_name="New group"))
+        app.group.create(group)
+        old_groups = app.group.get_group_list()
+    group.group_id = old_groups[0].group_id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    old_groups_with_id = sorted([g for g in old_groups if g.group_id is not None], key=Group.id_or_max)
+    new_groups_with_id = sorted([g for g in new_groups if g.group_id is not None], key=Group.id_or_max)
+    assert len(old_groups_with_id) == len(new_groups_with_id)
+    if old_groups_with_id:
+        old_groups_with_id[0] = group
+    assert old_groups_with_id == new_groups_with_id
 
 
 def test_modify_first_group_header(app):
+    old_groups = app.group.get_group_list()
+    group = Group(group_header="New header")
     if app.group.count() == 0:
-        app.group.create(Group(group_header="test"))
-    app.group.modify_first_group(Group(group_header="New header"))
+        app.group.create(group)
+        old_groups = app.group.get_group_list()
+    group.group_id = old_groups[0].group_id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    old_groups_with_id = sorted([g for g in old_groups if g.group_id is not None], key=Group.id_or_max)
+    new_groups_with_id = sorted([g for g in new_groups if g.group_id is not None], key=Group.id_or_max)
+    assert len(old_groups_with_id) == len(new_groups_with_id)
+    if old_groups_with_id:
+        old_groups_with_id[0] = group
+    assert old_groups_with_id == new_groups_with_id
